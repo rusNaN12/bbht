74b5f22b9269afe8af3a6c710bdb3846f633d2eb
Ruslan <ruslanbudeci@gmail.com>
2019-12-03 23:54:56 +0200

    fix ssh connection

--- setup.sh
+++ setup.sh
@@ -11,3 +11,9 @@ usermod -aG sudo $1
 cp -r ~/.ssh /home/$1
 chown -R $1:$1 /home/$1/.ssh
 
+echo "fix ssh persistend connection"
+echo "TCPKeepAlive yes" >> /etc/ssh/sshd_config 
+echo "ClientAliveInterval 30" >> /etc/ssh/sshd_config 
+echo "ClientAliveCountMax 99999" >> /etc/ssh/sshd_config
+
+service sshd restart
